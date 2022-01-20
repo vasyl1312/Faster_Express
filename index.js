25e770f0ab3b75f80209fb834d757f91748d7ce5
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-20 16:09:10 +0200

    simple server

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import express from "express";
 
+const PORT = process.env.PORT ?? 3000;//якщо є ПОРТ то беремо- як ні то 3000
 const app = express();
+
+app.listen(PORT, () => {
+    console.log(`Server has been started on ${PORT}...`);
+});
