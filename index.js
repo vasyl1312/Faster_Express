e22473e7750d168e00fea68c3c2e1f2f95b95dff
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-25 18:16:28 +0200

    add light to selected page

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'ejs'));
 
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Main page'})
+    res.render('index', {title: 'Main page', active: 'main'})
 })
 
 app.get('/features', (req, res) => {
-    res.render('features', {title: 'Features'})
+    res.render('features', {title: 'Features', active: 'features'})
 })
 // app.get('/', (req, res) => {
 //     //resolve(шлях текущої діректорії, в папці, файл)
