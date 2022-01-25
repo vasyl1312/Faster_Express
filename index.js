d05bc63b8d941306e29607f2df87b0f7d49f1352
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-25 14:09:08 +0200

    add index.html

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import path from "path";
 
+const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000; //якщо є ПОРТ то беремо- як ні то 3000
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Express.js!</h1>');
+    //res.send('<h1>Hello Express.js!</h1>');
+    //resolve(шлях текущої діректорії, в папці, файл)
+    res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 });
 
 app.listen(PORT, () => {
