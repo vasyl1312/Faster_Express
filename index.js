16b2817e73be6402cdd78e38a059f27233cf6c83
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-25 18:57:27 +0200

    add controllers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import {requestTime, logger} from "./middlewares.js";
+import serverRoutes from "./routes/servers.js"
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000; //якщо є ПОРТ то беремо- як ні то 3000
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(requestTime);//for middlewares
 app.use(logger);
+app.use(serverRoutes);
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'ejs'));
@@ -20,16 +22,7 @@ app.get('/', (req, res) => {
 app.get('/features', (req, res) => {
     res.render('features', {title: 'Features', active: 'features'})
 })
-// app.get('/', (req, res) => {
-//     //resolve(шлях текущої діректорії, в папці, файл)
-//     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
-// });
-
-// app.get('/download', (req, res) => {
-//     //download file
-//     res.download(path.resolve(__dirname, 'static', 'index.html'))
-// });
 
 app.listen(PORT, () => {
     console.log(`Server has been started on ${PORT}...`);
-});//14-50
+});//45-00
