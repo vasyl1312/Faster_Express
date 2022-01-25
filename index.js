504c984ca703297d688a0a7a3ccf8c53af422452
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-25 17:42:46 +0200

    add ejs

--- index.js
+++ index.js
@@ -10,15 +10,25 @@ app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(requestTime);//for middlewares
 app.use(logger);
 
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'ejs'));
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/features', (req, res) => {
+    res.render('features')
+})
 // app.get('/', (req, res) => {
 //     //resolve(шлях текущої діректорії, в папці, файл)
 //     res.sendFile(path.resolve(__dirname, 'static', 'index.html'))
 // });
 
-app.get('/download', (req, res) => {
-    //download file
-    res.download(path.resolve(__dirname, 'static', 'index.html'))
-});
+// app.get('/download', (req, res) => {
+//     //download file
+//     res.download(path.resolve(__dirname, 'static', 'index.html'))
+// });
 
 app.listen(PORT, () => {
     console.log(`Server has been started on ${PORT}...`);
