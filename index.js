4fb5644c8eddc1d8b27ee100c31b33f53b8fc541
Vasyl Hryts <vasylhryts74@gmail.com>
2022-01-25 14:02:09 +0200

    nodemon and server in chrome

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from "express";
 
-const PORT = process.env.PORT ?? 3000;//якщо є ПОРТ то беремо- як ні то 3000
+const PORT = process.env.PORT ?? 3000; //якщо є ПОРТ то беремо- як ні то 3000
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Express.js!</h1>');
+});
 
 app.listen(PORT, () => {
     console.log(`Server has been started on ${PORT}...`);
-});
+});//14-50
